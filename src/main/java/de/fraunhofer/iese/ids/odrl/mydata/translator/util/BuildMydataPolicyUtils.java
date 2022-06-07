c1a26ba0176df0112038cc70f6d4400d0195b520
Brandstaedter <Robin.Brandstaedter@iese.fraunhofer.de>
2022-06-07 11:05:55 +0200

    pom for IM4

--- src/main/java/de/fraunhofer/iese/ids/odrl/mydata/translator/util/BuildMydataPolicyUtils.java
+++ src/main/java/de/fraunhofer/iese/ids/odrl/mydata/translator/util/BuildMydataPolicyUtils.java
@@ -65,7 +65,7 @@ public class BuildMydataPolicyUtils {
 	public static String getName(String uri) {
 		// sample: http://example.com/policy/notify
 		String[] termSplit = uri.toString().split("/");
-		return termSplit[6];
+		return termSplit[termSplit.length-1];
 	}
 
 }
